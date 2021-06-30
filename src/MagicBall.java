82e1df84cd918179a335c20f428454f7a1a129af
Nisarg Vadgama <nisargvv@gmail.com>
2021-06-29 20:37:54 -0400

    Add random number generator to print random quotes

--- src/MagicBall.java
+++ src/MagicBall.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class MagicBall {
@@ -9,7 +10,20 @@ public class MagicBall {
         scan.next();
         scan.close();
 
-        System.out.println("As I see it, yes");
+        Random r = new Random();
+        int randomNum = r.nextInt(5);
+
+        if (randomNum == 0) {
+            System.out.println("Signs point to yes");
+        } else if (randomNum == 1) {
+            System.out.println("As I see it, yes");
+        } else if (randomNum == 2) {
+            System.out.println("Reply hazy, try again");
+        } else if (randomNum == 3) {
+            System.out.println("Don't count on it");
+        } else {
+            System.out.println("Outlook not so good");
+        }
     }
 
 }
