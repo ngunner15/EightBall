b76db4023dd789c5b827f3cef977fd56f9ada47c
Nisarg Vadgama <nisargvv@gmail.com>
2021-06-29 20:29:17 -0400

    Add Scanner to type question

--- src/MagicBall.java
+++ src/MagicBall.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class MagicBall {
 
     public static void main(String[] args) {
         System.out.println("Enter your question");
+
+        Scanner scan = new Scanner(System.in);
+        scan.next();
+        scan.close();
+
+        System.out.println("As I see it, yes");
     }
 
 }
